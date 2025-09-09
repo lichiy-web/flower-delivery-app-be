75e9b5e32815eeb0fdee2891ebcbd61ae2a7ef87
Yevhen Lykhosherstov <lichiy.web.dvlp@gmail.com>
2025-09-09 18:48:14 +0300

    feature/add-controller-error-wrapper-utility

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { rootController } from '../controllers/rootController.js';
 import orderRouter from './orders.js';
+import { ctrlErrWrapper } from '../utils/ctrlErrWrapper.js';
 
 const router = new Router();
 
-router.get('/', rootController);
+router.get('/', ctrlErrWrapper(rootController));
 router.use('/order', orderRouter);
 
 export default router;
--- src/routers/orders.js
+++ src/routers/orders.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import { createOrderController } from '../controllers/orders.js';
+import { ctrlErrWrapper } from '../utils/ctrlErrWrapper.js';
 
 const router = new Router();
 
-router.post('/', createOrderController);
+router.post('/', ctrlErrWrapper(createOrderController));
 
 export default router;
