b9a4a4c2d71a33d694d428d8196955baa193e139
Yevhen Lykhosherstov <lichiy.web.dvlp@gmail.com>
2025-09-09 12:00:43 +0300

    init/add-shop-mongoose-schema

---
+++ src/db/models/Shop.js
@@ -0,0 +1,34 @@
+import { Schema, model } from 'mongoose';
+
+const ShopSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    description: {
+      type: String,
+    },
+    address: {
+      type: String,
+      maxlength: 120,
+    },
+    phone: {
+      type: String,
+    },
+    logoUrl: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+export const Shop = model('Shop', ShopSchema);
